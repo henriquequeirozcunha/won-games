de1c2399657182899f033db5ceaa78a3a66a0c98
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-08-16 22:50:01 -0300

    test: add hideOnMobile prop to Logo SVG

--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import 'jest-styled-components'
+
 import Logo from '.'
 
 describe('<Logo />', () => {
@@ -26,9 +28,20 @@ describe('<Logo />', () => {
   })
 
   it('should render a normal logo when size is default', () => {
-    renderWithTheme(<Logo size="large" />)
+    renderWithTheme(<Logo size="normal" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
     })
+  })
+
+  it('should render a logo without text on hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })

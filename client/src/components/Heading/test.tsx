ba5cccf5d3ab802fa5bc6543b49290629ad5d02f
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-09-07 18:55:42 -0300

    refactor: adapt Heading Component to show different line colors

--- client/src/components/Heading/test.tsx
+++ client/src/components/Heading/test.tsx
@@ -22,13 +22,57 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a heading with a small font size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.4rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a heading with a medium font size', () => {
+    renderWithTheme(<Heading size="medium">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+  })
+
+  it('should render a heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineBottom lineLeft>
+        Won Games
+      </Heading>
+    )
+
+    const headingElement = screen.getByRole('heading', { name: /won games/i })
+
+    expect(headingElement).toHaveStyle({
+      'border-left': '0.7rem solid #F231A5'
+    })
+
+    expect(headingElement).toHaveStyleRule(
       'border-bottom',
       '0.5rem solid #F231A5',
       {

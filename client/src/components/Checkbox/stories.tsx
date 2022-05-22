11c21ff69199cef0d6cbc18d09e0597b53bfe48b
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-05-22 20:27:29 -0300

    feat: add Checkbox component

---
+++ client/src/components/Checkbox/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react'
+import Checkbox, { CheckboxProps } from '.'
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+  argTypes: {
+    onCheck: { action: 'checked' }
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox label="Action" labelFor="action" isChecked {...args} />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox label="Adventure" labelFor="adventure" {...args} />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox label="Strategy" labelFor="strategy" {...args} />
+    </div>
+  </>
+)

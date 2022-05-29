eaa22c0cbd6e9b88a79f0996bb944bdc4bdf883e
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-05-29 19:44:49 -0300

    feat: add form sign up

---
+++ client/src/components/FormSignUp/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import FormSignUp from '.'
+
+export default {
+  title: 'FormSignUp',
+  component: FormSignUp
+} as Meta
+
+export const Default: Story = () => <FormSignUp />

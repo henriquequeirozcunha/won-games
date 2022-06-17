329190883984fe9330d128c6c31bc8868652c950
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-06-17 00:23:22 -0300

    refactor: add GameInfo on Game template

--- client/src/templates/Game/styles.ts
+++ client/src/templates/Game/styles.ts
@@ -1,10 +1,18 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+import { Container } from 'components/Container'
+
+export const Main = styled.main`
+  margin-top: 20rem;
+  ${media.greaterThan('medium')`
+    margin-top: 58rem;
+  `}
+`
+
 type CoverProps = {
   src: string
 }
-
 export const Cover = styled.div<CoverProps>`
   ${({ src }) => css`
     position: absolute;
@@ -12,15 +20,24 @@ export const Cover = styled.div<CoverProps>`
     right: 0;
     left: 0;
     height: 39.5rem;
-
     background-image: url(${src});
     background-size: cover;
     background-position: top center;
     opacity: 0.4;
-
     ${media.greaterThan('medium')`
       height: 70rem;
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
     `}
   `}
 `
+
+const Section = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xlarge};
+    ${media.greaterThan('medium')`
+      margin-bottom: calc(${theme.spacings.xlarge} * 2);
+    `}
+  `}
+`
+
+export const SectionGameInfo = styled(Section)``

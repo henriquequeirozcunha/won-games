21975c65edba0f4ed88efd8facb35c0480554e09
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-06-17 01:00:48 -0300

    refactor: add GameDetails to Game template

--- client/src/components/GameDetails/index.tsx
+++ client/src/components/GameDetails/index.tsx
@@ -8,6 +8,7 @@ type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
 
 export type GameDetailsProps = {
   developer: string
+  publisher: string
   platforms: Platform[]
   releaseDate: string
   rating: Rating
@@ -16,6 +17,7 @@ export type GameDetailsProps = {
 
 const GameDetails = ({
   developer,
+  publisher,
   platforms,
   releaseDate,
   rating,
@@ -64,7 +66,7 @@ const GameDetails = ({
 
         <S.Block>
           <S.Label>Publisher</S.Label>
-          <S.Description>2k</S.Description>
+          <S.Description>{publisher}</S.Description>
         </S.Block>
 
         <S.Block>
--- client/src/templates/Game/index.tsx
+++ client/src/templates/Game/index.tsx
@@ -1,4 +1,5 @@
 import Gallery, { GalleryImageProps } from 'components/Gallery'
+import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import TextContent from 'components/TextContent'
 import Base from 'templates/Base'
@@ -9,9 +10,16 @@ export type GameTemplateProps = {
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
   description: string
+  details: GameDetailsProps
 }
 
-const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
+const Game = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+  details
+}: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -27,6 +35,10 @@ const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
       <S.SectionDescription>
         <TextContent title="Description" content={description} />
       </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
     </S.Main>
   </Base>
 )

826fb38bf073d814873a3fa97754423bcf0fafcc
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-06-18 16:37:31 -0300

    refactor: add wish list component

---
+++ client/src/components/Wishlist/index.tsx
@@ -0,0 +1,40 @@
+import { Container } from 'components/Container'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
+import Base from 'templates/Base'
+
+import * as S from './styles'
+
+export type WishlistTemplateProps = {
+  games?: GameCardProps[]
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+
+      {games?.map((game, index) => (
+        <GameCard key={`wishlist-${index}`} {...game} />
+      ))}
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
+export default Wishlist
---
+++ client/src/pages/wishlist.tsx
@@ -0,0 +1,17 @@
+import Wishlist, { WishlistTemplateProps } from 'components/Wishlist'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function WishlistPage(props: WishlistTemplateProps) {
+  return <Wishlist {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      games: gamesMock,
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighlight: highlightMock
+    }
+  }
+}

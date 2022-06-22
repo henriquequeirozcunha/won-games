6620a6fdd31b393842de272f2bea429b55770c88
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-06-22 02:09:49 -0300

    fix: remove border bottom when is not desktop

--- client/src/components/ProfileMenu/styles.ts
+++ client/src/components/ProfileMenu/styles.ts
@@ -4,7 +4,6 @@ import media from 'styled-media-query'
 export const Nav = styled.nav`
   ${({ theme }) => css`
     display: flex;
-    border-bottom: 0.1rem solid ${theme.colors.lightGray};
     ${media.greaterThan('medium')`
       flex-direction: column;
       border: 0;

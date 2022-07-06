e69d0371fdd5bc4797efe9164f5e2ae3885763b6
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-06 00:15:52 -0300

    feat: add Types generator

--- client/src/pages/games.tsx
+++ client/src/pages/games.tsx
@@ -1,15 +1,35 @@
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/tests/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: game.cover && game.cover.url,
+        price: new Intl.NumberFormat('en', {
+          currency: 'USD',
+          style: 'currency'
+        }).format(game.price)
+      })),
       filterItems: filterItemsMock
     }
   }

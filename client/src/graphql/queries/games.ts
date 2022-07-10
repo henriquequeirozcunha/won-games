3af8500edce7b87cfc0410824b4f810d6b47a066
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-09 21:24:28 -0300

    refactor: use apollo client, code fixes and improvements

--- client/src/graphql/queries/games.ts
+++ client/src/graphql/queries/games.ts
@@ -15,3 +15,35 @@ export const QUERY_GAMES = gql`
     }
   }
 `
+
+export const QUERY_GAME_BY_SLUG = gql`
+  query QueryGameBySlug($slug: String!) {
+    games(where: { slug: $slug }) {
+      name
+      short_description
+      description
+      cover {
+        src: url
+      }
+      price
+      rating
+      release_date
+      gallery {
+        src: url
+        label: alternativeText
+      }
+      developers {
+        name
+      }
+      publisher {
+        name
+      }
+      categories {
+        name
+      }
+      platforms {
+        name
+      }
+    }
+  }
+`
--- client/src/templates/Profile/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import ProfileTemplate from '.'
-
-export default {
-  title: 'ProfileTemplate',
-  component: ProfileTemplate
-} as Meta
-
-export const Default: Story = () => <ProfileTemplate />

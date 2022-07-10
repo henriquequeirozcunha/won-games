6ac812c5c919ea65ac230029a5cc89cedeefa10e
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-10 00:09:36 -0300

    feat: add dynamic home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,34 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import hilightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/tests/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: banner.image?.url,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonlink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon?.text,
+          ribbonColor: banner.ribbon?.color,
+          ribbonSize: banner.ribbon?.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: hilightMock,
       mostPopularGames: gamesMock,

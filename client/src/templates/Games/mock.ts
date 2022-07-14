4e0d56d8d1a86fce8e34d56fe67a4a68c6c4551c
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-13 22:22:44 -0300

    refactor: do not render ShowMore if there is no data to show

--- client/src/templates/Games/mock.ts
+++ client/src/templates/Games/mock.ts
@@ -1,5 +1,21 @@
 import { QUERY_GAMES } from 'graphql/queries/games'
 
+export const noGamesMock = {
+  request: {
+    query: QUERY_GAMES,
+    variables: { limit: 15, where: {} }
+  },
+  result: {
+    data: {
+      games: [],
+      gamesConnection: {
+        values: [],
+        __typename: 'GameConnection'
+      }
+    }
+  }
+}
+
 export const gamesMock = {
   request: {
     query: QUERY_GAMES,
@@ -18,11 +34,14 @@ export const gamesMock = {
           },
           __typename: 'Game'
         }
-      ]
+      ],
+      gamesConnection: {
+        values: [{ id: '1' }, { id: '2' }],
+        __typename: 'GameConnection'
+      }
     }
   }
 }
-
 export const fetchMoreMock = {
   request: {
     query: QUERY_GAMES,
@@ -41,7 +60,11 @@ export const fetchMoreMock = {
           },
           __typename: 'Game'
         }
-      ]
+      ],
+      gamesConnection: {
+        values: [{ id: '1' }, { id: '2' }],
+        __typename: 'GameConnection'
+      }
     }
   }
 }

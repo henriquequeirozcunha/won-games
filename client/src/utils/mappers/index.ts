316d5fc88d9597b9eba9a77512fb4a39ccc7759d
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-14 01:45:38 -0300

    refactor: code improvements

--- client/src/utils/mappers/index.ts
+++ client/src/utils/mappers/index.ts
@@ -3,7 +3,6 @@ import {
   QueryHome_banners,
   QueryHome_sections_freeGames_highlight
 } from 'graphql/generated/QueryHome'
-
 export const bannerMapper = (banners: QueryHome_banners[]) => {
   return banners.map((banner) => ({
     img: banner.image?.url,
@@ -20,30 +19,30 @@ export const bannerMapper = (banners: QueryHome_banners[]) => {
 }
 
 export const gamesMapper = (games: QueryGames_games[] | null | undefined) => {
-  return (
-    games &&
-    games.map((game) => ({
-      title: game.name,
-      slug: game.slug,
-      developer: game.developers[0].name,
-      img: game.cover?.url,
-      price: game.price
-    }))
-  )
+  return games
+    ? games.map((game) => ({
+        id: game.id,
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: game.cover?.url,
+        price: game.price
+      }))
+    : []
 }
 
 export const highlightMapper = (
   highlight: QueryHome_sections_freeGames_highlight | null | undefined
 ) => {
-  return (
-    highlight && {
-      title: highlight.title,
-      subtitle: highlight.subtitle,
-      backgroundImage: highlight.background?.url,
-      floatImage: highlight.floatImage?.url,
-      buttonLabel: highlight.buttonLabel,
-      buttonLink: highlight.buttonLink,
-      alignment: highlight.alignment
-    }
-  )
+  return highlight
+    ? {
+        title: highlight.title,
+        subtitle: highlight.subtitle,
+        backgroundImage: highlight.background?.url,
+        floatImage: highlight.floatImage?.url,
+        buttonLabel: highlight.buttonLabel,
+        buttonLink: highlight.buttonLink,
+        alignment: highlight.alignment
+      }
+    : {}
 }

2c77a77990c8d848be2145ef7b280b5e05c1ec4f
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-30 15:12:13 -0300

    feat: add Mutation Register and apply on FormSignUp

--- client/src/components/TextField/test.tsx
+++ client/src/components/TextField/test.tsx
@@ -24,8 +24,14 @@ describe('<TextField />', () => {
   })
 
   it('Changes its value when typing', async () => {
-    const onInput = jest.fn()
-    render(<TextField onInput={onInput} label="TextField" name="TextField" />)
+    const onInputChange = jest.fn()
+    render(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="TextField"
+      />
+    )
 
     const input = screen.getByRole('textbox')
     const text = 'This is my new text'
@@ -33,9 +39,9 @@ describe('<TextField />', () => {
 
     await waitFor(() => {
       expect(input).toHaveValue(text)
-      expect(onInput).toHaveBeenCalledTimes(text.length)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
-    expect(onInput).toHaveBeenCalledWith(text)
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 
   it('Is accessible by tab', () => {

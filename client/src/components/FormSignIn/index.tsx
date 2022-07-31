cdb3b7ef142091b4b6c5203b9542ec4f42b0cc1d
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-07-31 13:57:30 -0300

    feat: add Protected Routes

--- client/src/components/FormSignIn/index.tsx
+++ client/src/components/FormSignIn/index.tsx
@@ -20,7 +20,8 @@ const FormSignIn = () => {
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState(defaultValues)
   const [loading, setLoading] = useState(false)
-  const { push } = useRouter()
+  const routes = useRouter()
+  const { push, query } = routes
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
@@ -44,7 +45,7 @@ const FormSignIn = () => {
     const result = await signIn('credentials', {
       ...values,
       redirect: false,
-      callbackUrl: '/'
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
     })
 
     if (result?.url) {

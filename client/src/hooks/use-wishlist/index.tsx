4a7eaf34a50f8b9e5273063da4ed593ac5731314
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-08-14 18:22:41 -0300

    feat: add removeFromWishlist

--- client/src/hooks/use-wishlist/index.tsx
+++ client/src/hooks/use-wishlist/index.tsx
@@ -63,7 +63,7 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
     }
   )
 
-  const { data, loading } = useQueryWishlist({
+  const { data, loading: loadingQuery } = useQueryWishlist({
     skip: !session?.user?.email,
     context: {
       session
@@ -102,7 +102,16 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
       }
     })
   }
-  const removeFromWishlist = () => null
+  const removeFromWishlist = (id: string) => {
+    updateList({
+      variables: {
+        input: {
+          where: { id: wishlistId },
+          data: { games: wishlistIds.filter((gameId) => gameId !== id) }
+        }
+      }
+    })
+  }
 
   return (
     <WishlistContext.Provider
@@ -111,7 +120,7 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
         isInWishlist,
         addToWishlist,
         removeFromWishlist,
-        loading
+        loading: loadingQuery || loadingCreate || loadingUpdate
       }}
     >
       {children}

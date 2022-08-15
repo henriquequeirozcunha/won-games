5651fa43c6709bf45aaee8fa39c74c61ca9c3a23
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-08-15 00:19:33 -0300

    refactor: add WishlistButton on GameInfo component

--- client/src/components/GameInfo/index.tsx
+++ client/src/components/GameInfo/index.tsx
@@ -1,10 +1,9 @@
-import Button from 'components/Button'
 import Heading from 'components/Heading'
 import Ribbon from 'components/Ribbon'
 import formatPrice from 'utils/format-price'
-import { FavoriteBorder } from 'styled-icons/material-outlined'
 import * as S from './styles'
 import CartButton from 'components/CartButton'
+import WishlistButton from 'components/WishlistButton'
 
 export type GameInfoProps = {
   id: string
@@ -22,10 +21,8 @@ const GameInfo = ({ id, title, price, description }: GameInfoProps) => (
     <S.Description>{description}</S.Description>
 
     <S.ButtonsWrapper>
-      <Button size="large" icon={<FavoriteBorder />} minimal>
-        Wishlist
-      </Button>
       <CartButton id={id} size="large" hasText />
+      <WishlistButton id={id} hasText size="large" />
     </S.ButtonsWrapper>
   </S.Wrapper>
 )

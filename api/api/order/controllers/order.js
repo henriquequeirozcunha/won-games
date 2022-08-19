5f653a6f9f8a51c32e2505cdf921c1c2190e3646
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-08-18 23:17:06 -0300

    feat: add createPaymentIntent method

--- api/api/order/controllers/order.js
+++ api/api/order/controllers/order.js
@@ -7,6 +7,39 @@
 
 module.exports = {
   createPaymentIntent: () => {
-    return "Hello World";
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validatedGame) {
+          games.push(validatedGame);
+        }
+      })
+    );
+
+    if (!games.length) {
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!",
+      };
+    }
+
+    const total = games.reduce((acc, game) => {
+      return acc + game.price;
+    }, 0);
+
+    if (total === 0) {
+      return {
+        freeGames: true,
+      };
+    }
+
+    return { total_in_cents: total * 100, games };
   },
 };

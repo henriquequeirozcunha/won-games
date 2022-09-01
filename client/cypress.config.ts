ae7145d524adb455d8343a0f4d59431bf0b46fbd
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-08-31 23:23:56 -0300

    feat: configure start-server-and-test and baseUrl

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {},
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000'
   }
 })
---
+++ client/cypress/e2e/home.cy.ts
@@ -0,0 +1,5 @@
+describe('Home Page', () => {
+  it('should render home section', () => {
+    cy.visit('/')
+  })
+})

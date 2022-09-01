d7136a36a883dc3c83208b5807de772a0f4b44f8
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-08-31 23:36:20 -0300

    feat: add tests to home Page

--- client/cypress/e2e/home.cy.ts
+++ client/cypress/e2e/home.cy.ts
@@ -1,5 +1,22 @@
 describe('Home Page', () => {
   it('should render home section', () => {
     cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /horizon zero dawn/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /huge promotion/i })
+      cy.findByRole('link', { name: /browse games/i })
+    })
   })
 })
